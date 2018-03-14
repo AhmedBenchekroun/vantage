9d28e11c1d2668fe42f8aeb11fc076b9f888a30c
AhmedBenchekroun <ahmedbenchekroun@hotmail.fr>
2018-03-14 16:37:46 -0400

    Created all models

--- project/facebook_ads/models.py
+++ project/facebook_ads/models.py
@@ -7,5 +7,16 @@ from django.db import models
 
 class Facebook_Ad(models.model):
     name = models.CharField(max_length=255)
-    image_url = models.CharField(max_length=255)
-    link_url = models.CharField(max_length=255)
+    image_url = models.ForeignKey(Image, on_delete=models.CASCADE)
+    link_url = models.ForeignKey(Link, on_delete=models.CASCADE)
+
+class Image(models.model):
+    url = models.CharField(max_length=255)
+    image_attribute = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class Link(models.model):
+    url = models.CharField(max_length=255)
+    link_attribute = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class Tag(models.model):
+    attribute = models.CharField(max_length=255)
