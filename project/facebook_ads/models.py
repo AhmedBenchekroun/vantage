b01bd416b98b9b341c10918e1c775466f2b4d8c4
AhmedBenchekroun <ahmedbenchekroun@hotmail.fr>
2018-03-14 16:26:03 -0400

    Created facebook_ad model

--- project/facebook_ads/models.py
+++ project/facebook_ads/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Facebook_Ad(models.model):
+    name = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=255)
+    link_url = models.CharField(max_length=255)
