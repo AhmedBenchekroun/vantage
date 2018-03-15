fe5ce8492d60b586ec48f8c89cb9101d2fe71e18
AhmedBenchekroun <ahmedbenchekroun@hotmail.fr>
2018-03-15 15:06:15 -0400

    Now URL shows all Ads in DB

--- project/facebook_ads/views.py
+++ project/facebook_ads/views.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Ad
 
 def index(request):
-    return HttpResponse("<h1>This is it!</h1>")
+    all_ads = Ad.objects.all()
+    html = ""
+    for ad in all_ads:
+        url = '/facebook_ads/' + str(ad.id) + "/"
+        html += '<a href="' + url + '">' + ad.name + "</a></ br>"
+    return HttpResponse(html)
 
 def find_ad_by_id(request, ad_id):
     return HttpResponse("<h1>Ad id:" + str(ad_id) + "</h1>")
