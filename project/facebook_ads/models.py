c3ade10d948154eb2eb7b8c598a63b6a504f0a96
AhmedBenchekroun <ahmedbenchekroun@hotmail.fr>
2018-03-19 16:50:43 -0400

    Fixed issues related to many to many relationship

--- project/facebook_ads/models.py
+++ project/facebook_ads/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
@@ -9,11 +8,20 @@ class Tag(models.Model):
 
 class Image(models.Model):
     url = models.CharField(max_length=255)
-    image_attribute = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    image_attribute = models.ForeignKey(Tag, on_delete=models.CASCADE, null=True)
 
 class Link(models.Model):
     url = models.CharField(max_length=255)
-    link_attribute = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    link_attribute = models.ForeignKey(Tag, on_delete=models.CASCADE, null=True)
+
+class Tag_Image(models.Model):
+    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class Tag_Link(models.Model):
+    link_id = models.ForeignKey(Link, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
 
 class Ad(models.Model):
     name = models.CharField(max_length=255)
